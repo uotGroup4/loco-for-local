9297564243ee12735b5d67ae271f7bd2bf8c126a
Shannon <shannon.nell92@gmail.com>
2021-09-15 13:57:20 -0400

    Schema typeDefs laid out

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
+// const { AuthenticationError } = require('apollo-server-express');
+// const { User } = require('../models');
+// const { signToken } = require('../utils/auth');
+
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+
+                return userData;
+            }
+            // throw new AuthenticationError('Not logged in');
         }
-    }
+    },
 };
+
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,42 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        vendorCount: Int
+        savedVendors: [Vendor]
+    }
+
+    type Vendor {
+        vendorId: String
+        title: String
+        website: String
+        image: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input  vendorInput {
+        vendorId: String
+        title: String
+        website: String
+        image: String
+    }
+
     type Query {
-        helloWorld: String
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveVendor(input: vendorInput!): User
+        removeVendor(vendorId: String!): User
     }
 `;
 
