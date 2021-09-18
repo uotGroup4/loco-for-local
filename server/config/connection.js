7d966cc8ccaa2a91e48b5d684ec13c0bf095c976
SaidHadad <saiddavid.hadad@gmail.com>
2021-09-18 14:37:16 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+const url =  'mongodb+srv://saidhadad:Superstr0ng@loco-for-local.fumoj.mongodb.net/loco-for-local?retryWrites=true&w=majority'
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/loco-for-local',
+  process.env.MONGODB_URI || url,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }).
-  then((db) => console.log('Mongodb is connected to', db.connection.host))
+  })
+  .then((db) => console.log('Mongodb is connected to', db.connection.host))
 
 module.exports = mongoose.connection;
