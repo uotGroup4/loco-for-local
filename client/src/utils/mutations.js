0565cbf13b56bd82a3fd66ac0cb7e7954f29cac5
Shannon <shannon.nell92@gmail.com>
2021-09-19 20:15:29 -0400

    Login Signup pages with mutations
    Rough outline, waiting for front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+// login user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// add user through signup page
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
