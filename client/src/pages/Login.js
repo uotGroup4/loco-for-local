d7be3dc69df9ca2c1c22c101c3969d9e27a7b081
sylviamarja <82944649+sylviamarja@users.noreply.github.com>
2021-09-20 21:48:58 +0200

    login/signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import { Link } from "react-router-dom";
 
 import Auth from '../utils/auth';
 
-const Login = (props) => {
+function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
 
@@ -19,13 +20,17 @@ const Login = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
+    //try {
+    //const { data } = await login({
+    //variables: { ...formState },
+    //});
     try {
-      const { data } = await login({
-        variables: { ...formState },
-      });
+      const mutationResponse = await login({ variables: { email: formState.email, password: formState.password } })
+      const token = mutationResponse.data.login.token
+
 
       Auth.login(data.login.token);
     } catch (e) {
@@ -33,20 +38,94 @@ const Login = (props) => {
     }
 
     // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    const handleChange = event => {
+      const { name, password } = event.target;
+      setFormState({
+        ...formState,
+        [email]: value,
+        [password]: value,
+      });
+    };
+
+    /* return (
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          {/* input - onChange={handleChange} goes in here */} /*
+         </form>
+        {error && <div>Login failed</div>}
+      </div>
+    );
   };
 
+  export default Login; */
   return (
     <div>
-        <form onSubmit={handleFormSubmit}> 
-            {/* input - onChange={handleChange} goes in here */}
-        </form>
-        {error && <div>Login failed</div>}
-    </div>
+      <Container>
+        <div className='heading'>
+          <Link to="/signup">
+            ← Go to Signup
+
+          </Link>
+          <h2 style={{ color: '#fff' }}>Login</h2>
+        </div>
+        <Form
+          style={{ marginTop: '4rem' }}
+          name='basic'
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <p style={{ textAlign: 'center' }}>Name</p>
+          <Form.Item
+            name='username'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <p style={{ textAlign: 'center' }}>Password</p>
+
+          <Form.Item
+            name='password'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 11,
+              span: 16,
+            }}
+          >
+            <Button type='danger' htmlType='submit'>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Container>
+    </div >
   );
-};
+}
 
 export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import { Link } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 
-const Signup = () => {
+function Signup(props) {
   const [formState, setFormState] = useState({
     username: '',
     email: '',
@@ -25,29 +26,129 @@ const Signup = () => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email, password: formState.password, firstName: formState.firstName, lastName: formState.lastName
+      }
 
-    // use try/catch instead of promises to handle errors
-    try {
-      // execute addUser mutation and pass in variable data from form
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
   };
 
-  return (
-    <div>
-        <form onSubmit={handleFormSubmit}>
-            
-        </form>
 
-        {error && <div>Signup failed</div>}
+
+
+
+  // use try/catch instead of promises to handle errors
+  /*try {
+    // execute addUser mutation and pass in variable data from form
+    const { data } = await addUser({
+      variables: { ...formState },
+    });
+
+    Auth.login(data.addUser.token);
+  } catch (e) {
+    console.error(e);
+  }
+}; */
+
+  return (
+    <div className="heading">
+      <Link to="/login">
+        ← Go to Login
+    </Link>
+
+      <Container>
+        <div className='heading'>
+          <h2 style={{ color: '#fff' }}>Signup</h2>
+        </div>
+        <Form
+          name='basic'
+          style={{ marginTop: '3rem' }}
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <p style={{ textAlign: 'center' }}>Name</p>
+
+          <Form.Item
+            name='username'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <p style={{ textAlign: 'center' }}>Email</p>
+
+          <Form.Item
+            name='email'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Email!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <p style={{ textAlign: 'center' }}>Password</p>
+
+          <Form.Item
+            name='password'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <p style={{ textAlign: 'center' }}>Re-Enter Password</p>
+
+          <Form.Item
+            name='password2'
+            wrapperCol={{}}
+            rules={[
+              {
+                required: true,
+                message: 'Password does not match!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 11,
+              span: 16,
+            }}
+          >
+            <Button type='danger' htmlType='submit'>
+              Submit
+          </Button>
+          </Form.Item>
+        </Form>
+      </Container>
     </div>
   );
-};
+}
 
 export default Signup;
