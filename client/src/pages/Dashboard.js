1589e2768898ac3d57398032594fe1ce75618629
Shannon <shannon.nell92@gmail.com>
2021-09-21 11:05:24 -0400

    Dashboard issues

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,78 +1,96 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+// import { Redirect, useParams } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import VendorList from '../components/VendorList';
 
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME, QUERY_VENDORS } from '../utils/queries';
-import { SAVE_VENDOR } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { removeVendorId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+// import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { REMOVE_VENDOR } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 
 const Dashboard = (props) => {
-    const { username: userParam } = useParams();
+    // const { username: userParam } = useParams();
+    const { loading, data } = useQuery(GET_ME);
+    const [removeVendor, { error }] = useMutation(REMOVE_VENDOR);
 
-    const [saveVendor] = useMutation(SAVE_VENDOR);
-    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-        variables: { username: userParam },
-    }, QUERY_VENDORS);
+    // const { loading, data } = useQuery(userParam ? GET_ME : {
+    //     variables: { username: userParam },
+    // });
 
-    const user = data?.me || data?.user || {};
-    const vendor = data?.vendors || [];
+    // const vendor = data?.vendors || [];
+
+    const userData = data?.me || {};
+    console.log(userData);
 
     // redirect to personal profile page if username is yours
-    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-        return <Redirect to="/home" />;
+    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    //     return <Redirect to="/dashboard" />;
+    // }
 
+    // if data isn't here yet, say loading
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-        if (loading) {
-            return <div>Loading...</div>;
+    // if (!userData?.username) {
+    //     return (
+    //         <h4>
+    //             You need to be logged in to see this. Use the navigation links above to
+    //             sign up or log in!
+    //         </h4>
+    //     );
+    // }
+
+    // funciton to accept vendors _id value as param and delete vendor from user db
+    const handleDeleteVendor = async (vendorId) => {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
         }
 
-        if (!user?.username) {
-            return (
-                <h4>
-                    You need to be logged in to see this. Use the navigation links above to
-                    sign up or log in!
-                </h4>
-            );
-        }
+        try {
+            const { data } = await removeVendor({
+                variables: { vendorId }
+            });
 
-        const handleClick = async () => {
-            try {
-                await saveVendor({
-                    variables: { id: vendor._id }, //vendor or vendors?
-                });
-            } catch (e) {
-                console.error(e);
+            console.log("vendor data:", data);
+
+            if (error) {
+                throw new Error('Something went wrong');
             }
-        };
+
+            //upon success, remove vendor's id from localstorage
+            removeVendorId(vendorId);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
 
-        return (
-            <div>
-                <div className="">
-                    <h2 className="">
-                        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+
+    return (
+        <div>
+            <div className="">
+                <h2 className="">
+                    Viewing {userData ? `${userData.username}'s` : 'your'} profile.
                 </h2>
+            </div>
 
-                    {userParam && (
-                        <button className="" onClick={handleClick}>
-                            Save Vendor
-                        </button>
-                    )}
-                </div>
-
-                <div className="">
-                    <div className="col-12 col-lg-3 mb-3">
-                        <VendorList
-                            username={user.username}
-                            vendors={user.vendors}
-                        />
-                    </div>
+            <div className="">
+                <div className="col-12 col-lg-3 mb-3">
+                    <VendorList
+                        username={userData.username}
+                        vendors={userData.vendors}
+                    />
                 </div>
             </div>
-        );
-    };
-}
+        </div>
+    );
+};
+
 
 export default Dashboard;
